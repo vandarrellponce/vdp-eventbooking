3ed109a83c6df53b59d8cd20547c89c0db2dae38
Van Darrell Ponce <vanponce91@gmail.com>
2020-11-02 17:07:05 +0800

    schemas and resolvers

--- src/app.js
+++ src/app.js
@@ -1,10 +1,39 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import expressGraphql from 'express-graphql'
+import { buildSchema } from 'graphql'
 
 const app = express()
 
 dotenv.config()
 app.use(express.json())
+
+const graphqlHttp = expressGraphql.graphqlHTTP
+app.use(
+	'/graphql',
+	graphqlHttp({
+		schema: buildSchema(`
+            type RootQuery{
+                events: [String!]!
+            }
+            type RootMutation{
+                createEvent(name: String) : String
+            }
+            schema{
+                query: RootQuery
+                mutation: RootMutation
+            }
+        `),
+		rootValue: {
+			events: () => ['a', 'b'],
+			createEvent: (args) => {
+				const eventName = args.name
+				return eventName
+			},
+		},
+		graphiql: true,
+	})
+)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
